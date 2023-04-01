6aa47768155d91609ece7f7bc5cc8684738099c8
cryptopig <vpig76@gmail.com>
2023-04-01 07:05:49 -0700

    asdoifj

--- app/routes.py
+++ app/routes.py
@@ -78,4 +78,4 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    return render_template('account.html', title='Account Details')
