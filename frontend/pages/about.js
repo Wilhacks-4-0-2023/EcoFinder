3de10c2e7a98f35c507acc44cc1490d724158da6
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2023-04-01 00:40:17 -0700

    hp
    half complete home page

---
+++ frontend/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function About() {
+
+<>
+
+<div className="about1">
+
+    <h1> Why EcoFinder? </h1>
+    <p> EcoFinder was created to incerease the popularity of events catered towards the battle of Climate Change </p>
+
+     <div className="about2"> 
+
+       <h1> How did we make EcoFinder </h1>
+         </div>    
+</div>
+
+</>
+
+
+
+}
