5fc06557abbee86e627f1e67cc52561c845fb6f8
cryptopig <vpig76@gmail.com>
2023-04-01 10:44:23 -0700

    c

--- app/routes.py
+++ app/routes.py
@@ -58,19 +58,23 @@ def register():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+        flash('You are already logged in.', 'danger')
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+
             return redirect(next_page) if next_page else redirect(url_for('home'))
     return render_template('login.html', title = 'Log In', form = form)
 
 @app.route("/logout")
 def logout():
     logout_user()
+    flash('Successfully logged out', 'success')
     return redirect(url_for('home'))
+
 
 
 @app.route("/account")
