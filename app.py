b5b154fbc3229690100ab81979f4cdfff886f95b
cryptopig <vpig76@gmail.com>
2023-03-31 21:48:38 -0700

    added basic app functionality

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-# init
+from flask import Flask, render_template, flash, url_for, redirect
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = 'ab9c32a070313d1343395f1cc4d5a669'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(24), unique = True, nullable = False)
