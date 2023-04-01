8b8e19367ce525e9821e151e7df474d1ac922b16
mc08 <mc08>
2023-04-01 10:36:54 -0700

    c

--- app/static/map.js
+++ app/static/map.js
@@ -11,6 +11,7 @@ function update() {
     document.getElementById("quitEvent").disabled = !creatingEvent
     document.getElementById("eventForm").hidden = !creatingEvent
 
+    // document.getElementById("tempPolygonStorage").
     // window.clearPolygon()
 }
 
