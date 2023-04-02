e298db130c33f5f93a3f4deccc75ce96627811cc
cryptopig <vpig76@gmail.com>
2023-04-01 17:00:13 -0700

    as

--- app/routes.py
+++ app/routes.py
@@ -21,8 +21,6 @@ def about():
 def events():
     values = getEventRows().data
     values = json.loads(values)
-    print(values[0]["content"])
-
         
     return render_template('events.html', title = 'Events List', values = values)
 
